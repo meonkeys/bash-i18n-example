c214839b068d0e23896dceb72a23ab799a23511c
albfan <albertofanjul@gmail.com>
2014-10-12 19:30:48 +0200

    splitting for ease maintain

---
+++ messages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo ... $"Hello!"
+echo ... $"Hello $USER!"
+
--- test.sh
+++ test.sh
@@ -2,11 +2,9 @@
 mkdir -p locale/es/LC_MESSAGES
 msgfmt -o locale/es/LC_MESSAGES/$0.mo es.po
 echo Default locale:
-echo ... $"Hello!"
-echo ... $"Hello $USER!"
+./messages.sh
 TEXTDOMAIN=$0
 TEXTDOMAINDIR=$(pwd)/locale
 echo Language set to Spanish:
-echo ... $"Hello!"
-echo ... $"Hello $USER!"
+./messages.sh
 
