69009bbae1887df19840f7f6f1d7df33ca5eafa3
albfan <albertofanjul@gmail.com>
2014-10-12 19:20:39 +0200

    how to use parameters

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 mkdir -p locale/es/LC_MESSAGES
-msgfmt -o locale/es/LC_MESSAGES/test.sh.mo es.po
+msgfmt -o locale/es/LC_MESSAGES/$0.mo es.po
 echo Default locale:
 echo ... $"Hello!"
+echo ... $"Hello $USER!"
 TEXTDOMAIN=$0
-TEXTDOMAINDIR=`pwd`/locale
+TEXTDOMAINDIR=$(pwd)/locale
 echo Language set to Spanish:
 echo ... $"Hello!"
+echo ... $"Hello $USER!"
+
